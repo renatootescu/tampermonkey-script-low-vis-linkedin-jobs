4fb4c3e0b8c1397e3159884f9620ee4ee3ecedcf
Renato <19210522+renatootescu@users.noreply.github.com>
2025-08-08 15:33:24 +0300

    Update linkedin url match

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.linkedin.com/jobs/*
+// @match        https://www.linkedin.com/jobs/search/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
 // @grant        none
 // ==/UserScript==
